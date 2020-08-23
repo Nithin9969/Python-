7581f620243f3742aa4335537ee3e9709da65bab
Nithin9969 <nithinreddy9696@gmail.com>
2020-08-23 15:10:53 -0400

    def stat

---
+++ if stat.py
@@ -0,0 +1,95 @@
+# def add(a,b):
+#     print(f"a:{a} b:{b}")
+#     c=a+b
+#     print(c)
+#     return c
+
+# n1 = int(input("enter a"))
+# n2 = int(input("enter b"))
+
+# sum = add(n1,n2)
+# print(f"{n1}+ {n2} = {sum}")
+
+# n3 = add(20,30)
+# print(f"{20} + {30} = {n3}")
+
+# sum = add(b=n1,a=n2)
+# print(f"{n1} + {n2} = {sum}")
+
+# def add(a,b):
+#     c=a+b
+#     return c
+# def sub(a,b):
+#     c=a-b
+#     return c
+# def mul(a,b):
+#     c=a*b
+#     return c
+# def mod(a,b):
+#     c=a%b
+#     return c
+# a = int(input("enter a :"))
+# b = int(input("enter b: "))
+# print(f"{a} + {b} = {add(a,b)}")
+
+# print(f"{a} - {b} = {sub(a,b)}")
+
+# def sub(c,d):
+#     print(f"c:{c} d:{d}")
+#     f=c-d
+#     return f
+# n3 = int(input("enter c value: "))
+# n4 = int(input("enter d value: "))
+
+# # n5 = sub(76,89)
+# # print(f"{76} - {89} = {n5}")
+
+# subt = sub(d = n3,c = n4)
+# print(f"{n3} - {n4} = {subt}")
+
+
+def mul(e,f):
+    print(f"e value: {e} f value: {f}")
+    g = e*f
+    return g
+
+n5 = int(input("enter e value :"))
+n6 = int(input("enter f value :"))
+
+mult = mul(4,7)
+print(f"{4} * {7} = {mult}")
+
+multi = mul(e=n6, f=n5)
+print(f"{n5} * {n6} = {multi}")
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
